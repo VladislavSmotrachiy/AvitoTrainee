660eedcf81966f8d916181522cea059191b2a3b3
ErrorV9 <vlad.nikitenkov95@gmail.com>
2021-09-09 23:36:43 +0300

    add cached

--- AvitoTrainee/NetworkManager.swift
+++ AvitoTrainee/NetworkManager.swift
@@ -17,28 +17,28 @@ class NetworkManager {
         guard let stringURL = url else { return }
         guard let url = URL(string: stringURL) else { return }
         
-        if let cache = StorageManager.shared.fetchContacts().first  {
-            complition(cache)
-            return
-        } else {
-            URLSession.shared.dataTask(with: url) {(data, response,error) in
-                guard let data = data, let response = response else {
-                    print(error?.localizedDescription ?? "No error description")
-                    return
-                }
-                
-                do {
-                    guard url == response.url else { return }
-                    let cocktail = try JSONDecoder().decode(Avito.self, from: data)
-                    StorageManager.shared.save(contact: cocktail)
-                    DispatchQueue.main.async {
-                        complition(cocktail)
+        if StorageManager.shared.timerCache() == false {
+            if let cache = StorageManager.shared.fetchContacts().first  {
+                complition(cache)
+                return
+            } else {
+                URLSession.shared.dataTask(with: url) {(data, response,error) in
+                    guard let data = data, let response = response else {
+                        print(error?.localizedDescription ?? "No error description")
+                        return
                     }
-                } catch let error {
-                    print(error.localizedDescription)
-                }
-            }.resume()
+                    do {
+                        guard url == response.url else { return }
+                        let cocktail = try JSONDecoder().decode(Avito.self, from: data)
+                        StorageManager.shared.save(contact: cocktail)
+                        DispatchQueue.main.async {
+                            complition(cocktail)
+                        }
+                    } catch let error {
+                        print(error.localizedDescription)
+                    }
+                }.resume()
+            }
         }
     }
 }
-
--- AvitoTrainee/StorageManager.swift
+++ AvitoTrainee/StorageManager.swift
@@ -11,25 +11,35 @@ class StorageManager {
     
     static let shared = StorageManager()
     
+    let key = "date"
+    let date = Date().timeIntervalSince1970
     private let defaults = UserDefaults.standard
-    private let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    private let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last!
     private let archiveURL: URL
     private init() {
         archiveURL = documentDirectory.appendingPathComponent("First").appendingPathExtension("plist")
     }
     
+    func timerCache() -> Bool {
+        let saveDate = defaults.object(forKey: key) as! Double
+        let result =  saveDate - date > 3600
+        print(saveDate, result)
+        return result
+    }
+    
     func save(contact: Avito) {
-          var contacts = fetchContacts()
-          contacts.insert(contact, at: 0)
-          guard let data = try? PropertyListEncoder().encode(contacts) else { return }
-          try? data.write(to: archiveURL, options: .noFileProtection)
-          print("Сохранил" )
-      }
-      
-      func fetchContacts() -> [Avito] {
-          guard let data = try? Data(contentsOf: archiveURL) else { return [] }
-          guard let cocktail = try? PropertyListDecoder().decode([Avito].self, from: data) else { return [] }
-          print("получил")
-          return cocktail
-      }
+        var contacts = fetchContacts()
+        contacts.insert(contact, at: 0)
+        guard let data = try? PropertyListEncoder().encode(contacts) else { return }
+        try? data.write(to: archiveURL, options: .noFileProtection)
+        defaults.set(date, forKey: key)
+        print("Сохранил")
+    }
+    
+    func fetchContacts() -> [Avito] {
+        guard let data = try? Data(contentsOf: archiveURL) else { return [] }
+        guard let cocktail = try? PropertyListDecoder().decode([Avito].self, from: data) else { return [] }
+        print("Получил \(date)")
+        return cocktail
+    }
 }
